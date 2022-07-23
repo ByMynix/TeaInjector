d52decf6bf55156d2de4f1b686ebf7579fa94f75
ByMynix <j.r.riemer475@gmail.com>
2022-07-23 13:48:00 +0200

    Projektdateien hinzufügen.

---
+++ README.md
@@ -0,0 +1,50 @@
+# TeaInjector
+<<<<<<< Updated upstream
+![TeaInjector-Picture](https://bymynix.de/teainjector/assets/images/teainjector-picture-412x242.png?)
+
+[https://bymynix.de/teainjector/] 
+TeaInjector is an open-source project since 11.09.2021 and no longer closed-source. Please note that there is a file structure and it is not a single, let alone a Visual Studio project. The project is driven by a setup file [Inno Setup Compiler]. Here I will publish the different components of TeaInjector.
+=======
+
+[![CI](https://github.com/ByMynix/TeaInjector/actions/workflows/main.yml/badge.svg)](https://github.com/ByMynix/TeaInjector/actions/workflows/main.yml)
+<a href="https://github.com/ByMynix/CouInjector/stargazers"><img alt="GitHub Stars" src="https://img.shields.io/github/stars/ByMynix/TeaInjector"></a>
+<a href="https://dsc.gg/bymynixde"><img alt="ByMynixDE Discord Server" src="https://img.shields.io/discord/908055039258550305?label=Discord&logo=Discord"></a> 
+<a href="https://github.com/ByMynix/TeaInjector/commits/master"><img alt="GitHub commit activity" src="https://img.shields.io/github/commit-activity/w/ByMynix/TeaInjector"></a>
+<img alt="Platform Windows" src="https://img.shields.io/badge/platform-Windows-0078d7.svg?style=plastic"></a>
+<a href="https://www.paypal.com/paypalme/bymynix"><img alt="PayPal" src="https://img.shields.io/badge/donate-PayPal-104098.svg?style=plastic&logo=PayPal"></a>
+
+
+
+<img alt="TeaInjector-Picture" src="https://bymynix.de/teainjector/assets/images/teainjector-picture-570x280.png" />
+ 
+[https://bymynix.de/teainjector/]
+TeaInjector is an injector for Team Fortress 2. It's an open-source project since 11.09.2021 and no longer closed-source. From the 23.07.2022 it's a real Git project, as the previous versions consisted of a file structure and I was too lazy to make the effort.
+
+
+
+
+# Compile
+In this project "TeaInjector" resources were embedded:
+
+The resource "Updater", a winforms application as auto-updater ([Branch-Auto-Updater](https://github.com/ByMynix/TeaInjector/tree/TeaInjector-Auto-Updater)), which takes care of automatic updates.
+
+The resource "VAC-ByPass", i.e. the VAC-ByPass file ([Branch-VAC-ByPass](https://github.com/ByMynix/TeaInjector/tree/TeaInjector-VAC-ByPass)).
+
+These two resources, which are already embedded in "TeaInjector" can of course be replaced by compiling the two applications mentioned and then embedding them as resources.
+
+
+[Compile it in "Release / x86"]
+
+[Compiled application to use is in "\bin\x86\Release"]
+
+
+
+
+
+
+
+
+
+# Previous versions
+Please note that up to version 1.3 there is a file structure and it is not a single, let alone one Visual Studio project, which cannot be compiled in a simple way to use. From version 1.4 onwards, it is without a Setup Compiler and "TeaInjector" can be easily compiled.
+>>>>>>> Stashed changes
---
+++ TeaInjector/Form1.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Diagnostics;
+using IWshRuntimeLibrary;
+using ReaLTaiizor.Enum.Poison;
+using ReaLTaiizor.Forms;
+using ReaLTaiizor.Controls;
+using Microsoft.Win32;
+
+namespace TeaInjector
+{
+    public partial class Form1 : PoisonForm
+    {
+        string AppPath = AppDomain.CurrentDomain.BaseDirectory;
+        public Form1()
+        {
+            InitializeComponent();
+            BorderStyle = ReaLTaiizor.Enum.Poison.FormBorderStyle.FixedSingle;
+            ShadowType = FormShadowType.AeroShadow;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (System.IO.File.Exists(AppPath + @"\Updater.exe"))
+                System.IO.File.Delete(AppPath + @"\Updater.exe");
+            else
+            {
+            }
+
+            var client = new WebClient();
+            if ("No Updates available!" == client.DownloadString("https://bymynix.de/teainjector/Update%20Checker%201.3.txt"))
+            {
+                poisonLabel4.Text = "No Updates available! You are currently using the latest version of TeaInjector";
+            }
+            else
+            {
+                System.IO.File.WriteAllBytes(AppPath + @"\Updater.exe", Properties.Resources.Updater);
+                PoisonMessageBox.Show(this, "New update available! The Updater will start automatically after clicking 'Ok'.", "TeaInjector: Update-Checker", MessageBoxButtons.OK, MessageBoxIcon.Question);
+                Process.Start(AppPath + @"\Updater.exe");
+                Application.Exit();
+            }
+            if (Properties.Settings.Default.ToggleChecked1 == "False")
+                poisonToggle1.Checked = false;
+            else
+            {
+                WshShell wsh = new WshShell();
+                IWshRuntimeLibrary.IWshShortcut shortcut = wsh.CreateShortcut(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\TeaInjector.lnk") as IWshRuntimeLibrary.IWshShortcut;
+                shortcut.TargetPath = AppPath + @"\TeaInjector.exe";
+                shortcut.WindowStyle = 1;
+                shortcut.WorkingDirectory = AppPath;
+                shortcut.IconLocation = AppPath + @"\TeaInjector.exe";
+                shortcut.Save();
+            }
+            if (Properties.Settings.Default.ToggleChecked == "False")
+                poisonToggle2.Checked = false;
+            else
+            {
+                Process.Start("http://dsc.gg/bymynixde");
+                Process.Start("https://bymynix.de/projects/");
+                Process.Start("https://github.com/ByMynix/TeaInjector");
+            }
+        }
+
+        private void poisonTile1_Click_1(object sender, EventArgs e)
+        {
+            poisonStyleManager1.Theme = poisonStyleManager1.Theme == ThemeStyle.Light ? ThemeStyle.Dark : ThemeStyle.Light;         
+        }
+
+        private void poisonLabel10_Click(object sender, EventArgs e)
+        {
+            PoisonMessageBox.Show(this, "=====================" + Environment.NewLine
++ "- Turn off any antivirus on your computer" + Environment.NewLine
++ "- Click on [Start VAC-ByPass]" + Environment.NewLine
++ "- Start TF2" + Environment.NewLine
++ "- Click on [Choose File and Inject]" + Environment.NewLine
++ "- Enjoy !" + Environment.NewLine
++ "" + Environment.NewLine
++ "--------------" + Environment.NewLine
++ "", "[How to Use]");
+        }
+
+        private void poisonLink3_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://bymynix.de/teainjector/");
+        }
+
+        private void PictureBox8_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://bymynix.de/projects/");
+        }
+
+        private void PictureBox2_Click(object sender, EventArgs e)
+        {
+            Process.Start("http://dsc.gg/bymynixde");
+        }
+
+        private void poisonLinkLabel1_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://github.com/ByMynix/TeaInjector");
+        }
+
+        private void poisonToggle2_Click(object sender, EventArgs e)
+        {
+            if (poisonToggle2.Checked == false)
+            {
+                Properties.Settings.Default.ToggleChecked = "False";
+                poisonToggle2.Checked = false;
+                Properties.Settings.Default.Save();
+            }
+            if (poisonToggle2.Checked == true)
+            {
+                Properties.Settings.Default.ToggleChecked = "True";
+                poisonToggle2.Checked = true;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        private void poisonToggle1_Click(object sender, EventArgs e)
+        {
+            if (poisonToggle1.Checked == false)
+            {
+                Properties.Settings.Default.ToggleChecked1 = "False";
+                poisonToggle1.Checked = false;
+                Properties.Settings.Default.Save();
+                if (System.IO.File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) + @"\TeaInjector.lnk"))
+                    System.IO.File.Delete(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) + @"\TeaInjector.lnk");
+            }
+
+            if (poisonToggle1.Checked == true)
+            {
+                Properties.Settings.Default.ToggleChecked1 = "True";
+                poisonToggle1.Checked = true;
+                Properties.Settings.Default.Save();
+
+                WshShell wsh = new WshShell();
+                IWshRuntimeLibrary.IWshShortcut shortcut = wsh.CreateShortcut(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\TeaInjector.lnk") as IWshRuntimeLibrary.IWshShortcut;
+                shortcut.TargetPath = AppPath + @"\TeaInjector.exe";
+                shortcut.WindowStyle = 1;
+                shortcut.WorkingDirectory = AppPath;
+                shortcut.IconLocation = AppPath + @"\TeaInjector.exe";
+                shortcut.Save();
+            }
+        }
+
+        private void poisonButton3_Click(object sender, EventArgs e)
+        {
+            string url = "https://bymynix.de/teainjector/Changelogs.txt";
+            try
+            {
+                using (System.Net.WebClient wc = new System.Net.WebClient())
+                {
+                    PoisonMessageBox.Show(this, wc.DownloadString(url), "Changelogs");
+                }
+            }
+            catch (Exception ex)
+            {
+                PoisonMessageBox.Show(this, ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void poisonButton2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Injection.Run(GetPathDLL()))
+                {
+                    PoisonMessageBox.Show(this, "Successfully injected!", "TeaInjector: Injection", MessageBoxButtons.OK, MessageBoxIcon.Question);
+                }
+                else
+                {
+                    PoisonMessageBox.Show(this, "Failed!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                PoisonMessageBox.Show(this, ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private static string GetPathDLL()
+        {
+            string dllPath = string.Empty;
+            using (OpenFileDialog fileDialog = new OpenFileDialog())
+            {
+                fileDialog.InitialDirectory = Directory.GetCurrentDirectory();
+                fileDialog.Filter = "Select your cheat (*.dll)|*.dll";
+                fileDialog.FilterIndex = 2;
+                fileDialog.RestoreDirectory = true;
+
+                if (fileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    dllPath = fileDialog.FileName;
+                }
+                else
+                {
+                    throw new System.ArgumentNullException("No File selected");
+                }
+            }
+
+            return dllPath;
+        }
+
+        private void poisonButton1_Click(object sender, EventArgs e)
+        {
+            foreach (var process in Process.GetProcessesByName("hl2"))
+            {
+                process.Kill();
+            }
+            foreach (var process in Process.GetProcessesByName("Steam"))
+            {
+                process.Kill();
+            }
+            foreach (var process in Process.GetProcessesByName("steamwebhelper"))
+            {
+                process.Kill();
+            }
+            foreach (var process in Process.GetProcessesByName("SteamService"))
+            {
+                process.Kill();
+            }
+
+            System.IO.File.WriteAllBytes(AppPath + @"\ServiceHub2.TaskRun.Microsoft.dll", Properties.Resources.VAC_ByPass);
+
+            string strSteamInstallPath = (string)Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Valve\Steam", "InstallPath", null);
+            var proc = Process.Start(strSteamInstallPath + @"\Steam.exe");
+
+                while (string.IsNullOrEmpty(proc.MainWindowTitle))
+            {       
+                proc.Refresh();
+            }
+            
+            try
+            {
+                if (VACByPassLoader.Run(GetPathVACDLL()))
+                {
+                    poisonLabel4.Text = "VAC-ByPass is now active";
+                    poisonLabel3.ForeColor = Color.Lime;
+                    poisonLabel3.Text = "VAC-ByPass-Status:  Active";
+                    poisonButton1.Enabled = false;
+                    System.Threading.Thread SteamExit = new System.Threading.Thread(WaitingforSteamExit);
+                    SteamExit.Start();
+                    while (SteamExit.IsAlive)
+                        Application.DoEvents();
+                    poisonLabel4.Text = "VAC-ByPass is now inactive";
+                    poisonLabel3.ForeColor = Color.Red;
+                    poisonLabel3.Text = "VAC-ByPass-Status:  Inactive";
+                    poisonButton1.Enabled = true;
+                }
+                else
+                {
+                    PoisonMessageBox.Show(this, "Failed!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                PoisonMessageBox.Show(this, ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private static string GetPathVACDLL()
+        { 
+            string dllPath = System.AppDomain.CurrentDomain.BaseDirectory + @"\ServiceHub2.TaskRun.Microsoft.dll";
+
+            return dllPath;
+        }
+        private void WaitingforSteamExit()
+        {
+            Process T = new Process();
+            {
+                var withBlock = T;
+                foreach (Process p1 in Process.GetProcessesByName("Steam"))
+                    p1.WaitForExit();
+            }
+        }
+    }
+}
