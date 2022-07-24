ed34e185533b725f33c52d3de6bc75c3b9c9967e
ByMynix <j.r.riemer475@gmail.com>
2022-07-24 17:50:08 +0200

    Projektdateien hinzufügen.

---
+++ README.md
@@ -0,0 +1,8 @@
+# TeaInjector-Auto-Updater
+
+![TeaInjector-Updater-Picture](https://bymynix.de/teainjector/TeaInjector-Auto-Updater-Picture.png?)
+
+# Compile
+Compile it in "Release / x86"
+
+Compiled application to use is in "\bin\x86\Release"
---
+++ TeaInjector Auto-Update/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Windows.Forms;
+
+namespace TeaInjector_Auto_Update
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Der Haupteinstiegspunkt für die Anwendung.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            try
+            {
+                using (var client = new WebClient())
+                using (client.OpenRead("http://google.com/generate_204"))
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+            }
+            catch (System.Net.WebException)
+            {
+                MessageBox.Show("Internet connection required! Either the connection is blocked or it doesn't exist. TeaInjector-Auto-Updater will now close", "TeaInjector-Auto-Updater Error");
+                Application.Exit();
+            }
+        }
+    }
+}
